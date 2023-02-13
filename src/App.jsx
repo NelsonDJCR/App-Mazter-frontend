3a697d20d757611ad8d37f82b02874b390e70220
Nelson Jimenez <dev@nelsondjcr.com>
2023-02-13 10:17:50 -0500

    Add useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import React from "react";
+import { useState } from "react";
 import "./App.css";
 import { Routing } from "./config/Routing";
 
+export const authContext = React.createContext();
+
 function App() {
+    const [user, setUser] = useState(null);
+
+    const changeLogin = () => {
+        if (user) {
+            setUser();
+        } else {
+            setUser({ name: "Nelson" });
+        }
+    };
+
     return (
         <div className="App">
-            <Routing />
+            <authContext.Provider value={user}>
+                <button onClick={changeLogin}> Change login</button>
+                <Routing />
+            </authContext.Provider>
         </div>
     );
 }
--- src/config/Routing.jsx
+++ src/config/Routing.jsx
@@ -1,11 +1,20 @@
 import { Route, Routes, Link } from "react-router-dom";
 import Index from "../components/Index";
 
+import { authContext } from "../App";
+import { useContext } from "react";
+
 const Home = () => {
+    const user = useContext(authContext);
     return (
         <>
             <Index />
             <h1>Home</h1>
+            {user ? (
+                <>
+                    <p>Hello {user.name}</p>
+                </>
+            ) : null}
         </>
     );
 };
