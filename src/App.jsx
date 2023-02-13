3b4ad19d41065455ec49cb70835311fd5ac2f5d4
Nelson Jimenez <dev@nelsondjcr.com>
2023-02-13 10:04:32 -0500

    Pull apart roting

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,10 @@
 import "./App.css";
-import { Route, Routes, Link, useParams, Outlet } from "react-router-dom";
-import Index from "./components/Index";
-const Tacos = () => {
-    const { name } = useParams();
-    return (
-        <>
-            <h1>Taco: {name}</h1>
-            <h3>
-                <Link to={"details"}> Detail</Link>
-            </h3>
-            <Outlet />
-        </>
-    );
-};
-const TacoDetail = () => {
-    const { name } = useParams();
-    return (
-        <>
-            <h3>Detail Taco {name} is.... gooood</h3>
-        </>
-    );
-};
-const Home = () => <h1>Home</h1>;
-const SearchPage = () => {
-    const tacos = ["Pastor", "Carnita", "Alambre"];
-    return (
-        <>
-            <h1>Search Page</h1>
-            <ul>
-                {tacos.map((taco) => (
-                    <li key={taco}>
-                        <Link to={`/tacos/` + taco}>{taco}</Link>
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
-};
-
-const NotFound = () => {
-    return (
-        <>
-            <h1>404 not Found</h1>
-            <h5></h5>
-        </>
-    );
-};
+import { Routing } from "./config/Routing.jsx";
 
 function App() {
     return (
         <div className="App">
-            <Index/>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/search-page" element={<SearchPage />} />
-                <Route path="/tacos/:name" element={<Tacos />}>
-                    <Route path="details" element={<TacoDetail />}></Route>
-                </Route>
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+            <Routing />
         </div>
     );
 }
---
+++ src/config/Routing.jsx
@@ -0,0 +1,38 @@
+import { Route, Routes, Link } from "react-router-dom";
+import Index from "../components/Index";
+
+const Home = () => {
+    return (
+        <>
+            <Index />
+            <h1>Home</h1>
+        </>
+    );
+};
+const SearchPage = () => {
+    return (
+        <>
+            <Index />
+            <h1>Search Page</h1>
+        </>
+    );
+};
+const NotFound = () => {
+    return (
+        <>
+            <Index />
+            <h1>404 not Found</h1>
+            <h5></h5>
+        </>
+    );
+};
+
+export const Routing = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search-page" element={<SearchPage />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
+};
