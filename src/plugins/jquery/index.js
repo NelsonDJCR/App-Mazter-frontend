390e532f7758ab902f03f9130ea7cdf0c79972e4
Nelson Jimenez <dev@nelsondjcr.com>
2023-02-14 13:39:30 -0500

    Delete files for clean

--- src/plugins/jquery/index.js
+++
@@ -1,9 +0,0 @@
-import $ from "jquery";
-
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= 30) {
-        $(".header").addClass("fixed-header");
-    } else {
-        $(".header").removeClass("fixed-header");
-    }
-});
