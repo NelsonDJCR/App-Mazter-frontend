fb237959e87b695259352db3206368ad9270dc51
Nelson Jimenez <dev@nelsondjcr.com>
2023-02-14 15:57:20 -0500

    Change context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export const AuthContext = React.createContext({});
 
 export default function AuthContextProvider({ children }) {
-    const [auth, setAuth] = useState(true);
+    const [auth, setAuth] = useState(false);
 
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
