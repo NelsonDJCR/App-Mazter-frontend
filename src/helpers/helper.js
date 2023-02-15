ff67aa26e3e26b34c0cdf666ea86e0ddaef5a484
Nelson Jimenez <dev@nelsondjcr.com>
2023-02-15 15:36:59 -0500

    Change url api

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -2,7 +2,7 @@ export function getBearer() {
     return localStorage.getItem("bearer");
 }
 export function urlApi(url) {
-    return "https://mazter-backend.develop/api/v1/" + url;
+    return "http://mazter-backend.test/api/v1/" + url;
     //  return "http://mazter-backend.develop/api/v1/" + url;
     // return 'https://api.nelsondjcr.com/api/v1/'+ url;
 }
