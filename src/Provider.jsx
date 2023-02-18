c983f876fc2420c68cb34016965701d611c58270
NelsonDJCR <dev@nelsondjcr.com>
2023-02-18 17:14:11 -0500

    Delete context

--- src/Provider.jsx
+++ src/Provider.jsx
@@ -1,9 +1,9 @@
-import AuthContextProvider from "./context/AuthContext";
-import UserContextProvider from "./context/UserContext";
+// import AuthContextProvider from "./context/AuthContext";
+// import UserContextProvider from "./context/UserContext";
 export default function Provider({ children }) {
     return (
-        <AuthContextProvider>
-            <UserContextProvider>{children}</UserContextProvider>
-        </AuthContextProvider>
+        
+           {children}
+        
     );
 }
